7621a03c53771bae3b6191864d6615cc858026c9
Gabriel Qualho <100730337+gaoliverq@users.noreply.github.com>
2022-04-21 16:09:49 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+//texto digitado
+var txtNormal = document.getElementById('textoNormal')
+//texto saida
+var txtCripto = document.getElementById('textoCripto')
+//botao cripto
+var btnC = document.getElementById('criptobtn')
+// botao descripto
+var btnD = document.getElementById('descriptobtn')
+
+textoNormal.focus()
+
+function criptografar(){
+    var texto = textoNormal.value; 
+    var resultado = texto.replace(/e/g,'enter')
+    var resultado1 = resultado.replace(/i/g, 'imes')
+    var resultado2 = resultado1.replace(/a/g, 'ai')
+    var resultado3 = resultado2.replace(/u/g, 'ufat')
+    var resultado4 = resultado3.replace(/o/g, 'ober')
+    
+    textoCripto.value = resultado4;
+}
+ function descriptografar(){
+     var texto = textoNormal.value;
+     var resultado = texto.replace(/enter/g, 'e')
+     var resultado1 = resultado.replace(/imes/g, 'i')
+     var resultado2 = resultado1.replace(/ai/g, 'a')
+     var resultado3 = resultado2.replace(/ufat/g, 'u')
+     var resultado4 = resultado3.replace(/ober/g, 'o')
+
+     textoCripto.value = resultado4;
+ }
+
+ function copy (){
+     var copy = textoNormal.value;
+     navigator.clipboard.writeText(copy);
+ }
+
+ 
+btnC.onclick = criptografar;
+
+btnD.onclick = descriptografar;
+
+
